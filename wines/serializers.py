60a54dad7e71d164e2d9fa5c523825dfd187c704
James Kelsey <jamesmskelsey@gmail.com>
2021-12-07 14:33:15 -0800

    convert to drf

--- wines/serializers.py
+++ wines/serializers.py
@@ -1,29 +1,36 @@
-from builtins import object
-
-class WineSerializer(object):
-    def __init__(self, body):
-        self.body = body
+from .models import Wine
+from rest_framework import serializers
 
 
-    @property
-    def all_wines(self):
-        output = {'wines': []}
-        for wine in self.body:
-            wine_details = {
-                'wine_name': wine.wine_name,
-                'price': wine.price,
-                'varietal': wine.varietal,
-                'description': wine.description
-            }
-            output['wines'].append(wine_details)
+class WineSerializer(serializers.ModelSerializer):
+    model = Wine
+    fields = ['wine_name', 'price', 'varietal', 'description']
 
-        return output
 
-    @property
-    def wine_detail(self):
-        return {
-            'wine_name': self.body.wine_name,
-            'price': self.body.price,
-            'varietal': self.body.varietal,
-            'description': self.body.description
-        }
+# class WineSerializer(object):
+#     def __init__(self, body):
+#         self.body = body
+
+
+#     @property
+#     def all_wines(self):
+#         output = {'wines': []}
+#         for wine in self.body:
+#             wine_details = {
+#                 'wine_name': wine.wine_name,
+#                 'price': wine.price,
+#                 'varietal': wine.varietal,
+#                 'description': wine.description
+#             }
+#             output['wines'].append(wine_details)
+
+#         return output
+
+#     @property
+#     def wine_detail(self):
+#         return {
+#             'wine_name': self.body.wine_name,
+#             'price': self.body.price,
+#             'varietal': self.body.varietal,
+#             'description': self.body.description
+#         }
--- wines/urls.py
+++ wines/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from rest_framework import urlpatterns
 from . import views
+from .views import WineViewSet
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path('', views.wine_list, name='wine_list'),
-    path('new', views.new_wine, name='new_wine'),
-    path('<int:wine_id>', views.wine_detail, name='wine_detail'),
-    path('<int:wine_id>/edit', views.edit_wine, name='edit_wine'),
-    path('<int:wine_id>/delete', views.delete_wine, name='delete_wine'),
-]
+router = SimpleRouter()
+router.register(r'', WineViewSet, basename="wines")
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', views.wine_list, name='wine_list'),
+#     path('new', views.new_wine, name='new_wine'),
+#     path('<int:wine_id>', views.wine_detail, name='wine_detail'),
+#     path('<int:wine_id>/edit', views.edit_wine, name='edit_wine'),
+#     path('<int:wine_id>/delete', views.delete_wine, name='delete_wine'),
+# ]
